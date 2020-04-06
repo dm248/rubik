63583f2f5c3db8353621ce574d9e6ae55b557e7e
Denes Molnar <dmolnar314@gmail.com>
2020-04-06 13:00:04 +0200

    Converted states to 0-53, faces to 0-5

--- rubik/rubik.py
+++ rubik/rubik.py
@@ -7,7 +7,7 @@
 # so do it ourselves, in similar notation as in there.
 #
 #
-# YBRGOW face colors: [Y]ellow ->1  [B]lue ->2  [R]ed ->3  [G]reen ->4  [O]range ->5  [W]hite -> 6
+# YBRGOW face colors: [Y]ellow ->0  [B]lue ->1  [R]ed ->2  [G]reen ->3  [O]range ->4  [W]hite -> 5
 #
 # Keep:     4 (Upper center) = YELLOW
 #          13 (Left center)  = BLUE
@@ -63,61 +63,50 @@ class RubikCube():
    # face/color coding
    ###
 
-   color2int_map_FACES = { 'Y': 1, 'B': 2, 'R': 3, 'G': 4, 'O': 5, 'W': 6 }
+   # face color -> face idx
+   facecolor2int_map = { 'Y': 0, 'B': 1, 'R': 2, 'G': 3, 'O': 4, 'W': 5 }   
+ 
+   # face idx -> face color
+   int2facecolor_map = {}
+   for (k,v) in facecolor2int_map.items():  int2facecolor_map[v] = k
 
-   color2int_map  = { 'a':  1, 'b':  2, 'c':  3, 'd':  4, 'e':  5,
-                      'f':  6, 'g':  7, 'h':  8, 'i':  9, 'j': 10,
-                      'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15,
-                      'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20,
-                      'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25,
-                      'z': 26, '0': 27, '1': 28, '2': 29, '3': 30,
-                      '4': 31, '5': 32, '6': 33, '7': 34, '8': 35,
-                      '9': 36, 'A': 37, 'B': 38, 'C': 39, 'D': 40,
-                      'E': 41, 'F': 42, 'G': 43, 'H': 44, 'I': 45,
-                      'J': 46, 'K': 47, 'L': 48, 'M': 49, 'N': 50,
-                      'O': 51, 'P': 52, 'Q': 53, 'R': 54
+   # state name -> state idx
+   state2int_map  = { 'a':  0, 'b':  1, 'c':  2, 'd':  3, 'e':  4,
+                      'f':  5, 'g':  6, 'h':  7, 'i':  8, 'j':  9,
+                      'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14,
+                      'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19,
+                      'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24,
+                      'z': 25, '0': 26, '1': 27, '2': 28, '3': 29,
+                      '4': 30, '5': 31, '6': 32, '7': 33, '8': 34,
+                      '9': 35, 'A': 36, 'B': 37, 'C': 38, 'D': 39,
+                      'E': 40, 'F': 41, 'G': 42, 'H': 43, 'I': 44,
+                      'J': 45, 'K': 46, 'L': 47, 'M': 48, 'N': 49,
+                      'O': 50, 'P': 51, 'Q': 52, 'R': 53
                     }
 
-   int2color_map_FACES = {  1: 'Y',  2: 'Y',  3: 'Y',  4: 'Y',  5: 'Y',
-                            6: 'Y',  7: 'Y',  8: 'Y',  9: 'Y', 10: 'B',
-                           11: 'B', 12: 'B', 13: 'B', 14: 'B', 15: 'B',
-                           16: 'B', 17: 'B', 18: 'B', 19: 'R', 20: 'R',
-                           21: 'R', 22: 'R', 23: 'R', 24: 'R', 25: 'R',
-                           26: 'R', 27: 'R', 28: 'G', 29: 'G', 30: 'G',
-                           31: 'G', 32: 'G', 33: 'G', 34: 'G', 35: 'G',
-                           36: 'G', 37: 'O', 38: 'O', 39: 'O', 40: 'O',
-                           41: 'O', 42: 'O', 43: 'O', 44: 'O', 45: 'O',
-                           46: 'W', 47: 'W', 48: 'W', 49: 'W', 50: 'W',
-                           51: 'W', 52: 'W', 53: 'W', 54: 'W'
-                        }
-   
-   int2color_map = {  1: 'a',  2: 'b',  3: 'c',  4: 'd',  5: 'e',
-                      6: 'f',  7: 'g',  8: 'h',  9: 'i', 10: 'j',
-                     11: 'k', 12: 'l', 13: 'm', 14: 'n', 15: 'o',
-                     16: 'p', 17: 'q', 18: 'r', 19: 's', 20: 't',
-                     21: 'u', 22: 'v', 23: 'w', 24: 'x', 25: 'y',
-                     26: 'z', 27: '0', 28: '1', 29: '2', 30: '3',
-                     31: '4', 32: '5', 33: '6', 34: '7', 35: '8',
-                     36: '9', 37: 'A', 38: 'B', 39: 'C', 40: 'D',
-                     41: 'E', 42: 'F', 43: 'G', 44: 'H', 45: 'I',
-                     46: 'J', 47: 'K', 48: 'L', 49: 'M', 50: 'N',
-                     51: 'O', 52: 'P', 53: 'Q', 54: 'R'
-                  }
+   # state idx -> state name
+   int2state_map = {}
+   for (k,v) in state2int_map.items(): int2state_map[v] = k
 
-   # color name to integer value
-   def color2int(c, c2imap = color2int_map):
+   # state idx -> face idx
+   def idx2face(i):
+      return (i // 9)
+
+   # state idx -> face color
+   int2state_map_FACES = {}
+   for i in range(n): int2state_map_FACES[i] = int2facecolor_map[idx2face(i)]
+   
+   # state name to integer value
+   def color2int(c, c2imap = state2int_map):
       return c2imap.get(c, 0)
 
    # color value to name
-   def int2color(v, i2cmap = int2color_map):
+   def int2color(v, i2cmap = int2state_map):
       return i2cmap.get(v, "*")
 
    ###
    # centers, corners and edges
    ###
-
-   def idx2face(i):
-      return (i // 9) + 1
 
    center_indices = (  4, 13, 22, 31, 40, 49 );   # U, L, F, R, B, D
 
@@ -180,15 +169,14 @@ class RubikCube():
          self.setState(RubikCube.string2state(s))
 
    def reset(self):
-      for i in range(self.n):
-         self.state[i] = i + 1
+      for i in range(self.n):  self.state[i] = i
 
    # state related
 
    # whether cube is in solved state - FIXME: is it faster to use isInState() ??
    def isReset(self):
       for i in range(self.n):
-         if self.state[i] != i + 1:
+         if self.state[i] != i:
             return False
       return True
 
@@ -214,37 +202,37 @@ class RubikCube():
    # printing
 
    def toString(self, facesonly = False):
-      i2cmap = RubikCube.int2color_map  if not facesonly  else RubikCube.int2color_map_FACES
+      i2cmap = RubikCube.int2state_map  if not facesonly  else RubikCube.int2state_map_FACES
       return "".join( [ RubikCube.int2color(v, i2cmap) for v in self.state ]  )
 
-   def __range2string(self, start, end, facesonly = False):
-      i2cmap = RubikCube.int2color_map  if not facesonly  else RubikCube.int2color_map_FACES
+   def _range2string(self, start, end, facesonly = False):
+      i2cmap = RubikCube.int2state_map  if not facesonly  else RubikCube.int2state_map_FACES
       return "".join( [ RubikCube.int2color(v, i2cmap) for v in self.state[start:end] ] )
 
    def print(self, facesonly = False):
       for row in range(3):   # top face
          start = row * 3
-         print("   |" + self.__range2string(start, start + 3, facesonly) + "|")
+         print("   |" + self._range2string(start, start + 3, facesonly) + "|")
       print("    ---")
       for row in range(3): # left, mid, right, back
          s = ""
          for face in range(1, 5): 
             start = face * 9 + row * 3
-            s += self.__range2string(start, start + 3, facesonly)
+            s += self._range2string(start, start + 3, facesonly)
             if face != 4:
                s += "|"
          print(s)
       print("    ---")
       for row in range(3):   # bottom face
          start = 5 * 9 + row * 3
-         print("   |" + self.__range2string(start, start + 3, facesonly) + "|")
+         print("   |" + self._range2string(start, start + 3, facesonly) + "|")
 
    #
    # basic moves (L R F B U D)
    #
 
    # cycle positions given by indices
-   def __cycleCells(self, shft, indices):
+   def _cycleCells(self, shft, indices):
       state = self.state
       tmp = tuple( state[idx] for idx in indices )
       ncycle = len(indices)
@@ -255,89 +243,89 @@ class RubikCube():
    # rotate one face clockwise:   012     630
    #                              345 ->  741
    #                              678     852
-   def __rotateFace(self, face, dir = 1):
+   def _rotateFace(self, face, dir = 1):
       pos = 9 * face   # top left corner of face
-      self.__cycleCells(2 * dir, (pos, pos + 1, pos + 2, pos + 5, pos + 8, pos + 7, pos + 6, pos + 3) )
+      self._cycleCells(2 * dir, (pos, pos + 1, pos + 2, pos + 5, pos + 8, pos + 7, pos + 6, pos + 3) )
          
     
    # left
    
-   def moveL(self, dir = 1): # clockwise turn of LEFT face (around #13)
-      self.__rotateFace(1, dir)   # face
+   def _moveL(self, dir = 1): # clockwise turn of LEFT face (around #13)
+      self._rotateFace(1, dir)   # face
       # neighbors: 0, 3, 6|18,21,24|45,48,51|44,41,38| -> rotate by 3 for CW
-      self.__cycleCells(3 * dir, (0, 3, 6, 18, 21, 24, 45, 48, 51, 44, 41, 38) )
+      self._cycleCells(3 * dir, (0, 3, 6, 18, 21, 24, 45, 48, 51, 44, 41, 38) )
 
-   def moveLinv(self):   # L'
-       self.moveL(-1)
+   def _moveLinv(self):   # L'
+       self._moveL(-1)
 
-   def moveL2(self):
-       self.moveL(dir = 2)
+   def _moveL2(self):
+       self._moveL(dir = 2)
 
    # right
  
-   def moveR(self, dir = 1): #clockwise turn of RIGHT face (around #31)
-      self.__rotateFace(3, dir)   # face
+   def _moveR(self, dir = 1): #clockwise turn of RIGHT face (around #31)
+      self._rotateFace(3, dir)   # face
       # neighbors: 53,50,47|26,23,20| 8, 5, 2|36,39,42| -> rotate by 3 for CW
-      self.__cycleCells(3 * dir, (53, 50, 47, 26, 23, 20, 8, 5, 2, 36, 39, 42) )
+      self._cycleCells(3 * dir, (53, 50, 47, 26, 23, 20, 8, 5, 2, 36, 39, 42) )
 
-   def moveRinv(self):       # R'
-       self.moveR(dir = -1)
+   def _moveRinv(self):       # R'
+       self._moveR(dir = -1)
 
-   def moveR2(self):
-       self.moveR(dir = 2)
+   def _moveR2(self):
+       self._moveR(dir = 2)
 
    # front
  
-   def moveF(self, dir = 1): #clockwise turn of FRONT face (around #22)
-      self.__rotateFace(2, dir)   # face
+   def _moveF(self, dir = 1): #clockwise turn of FRONT face (around #22)
+      self._rotateFace(2, dir)   # face
       # neighbors:  6, 7, 8|27,30,33|47,46,45|17,14,11| -> rotate by 3 for CW
-      self.__cycleCells(3 * dir, (6, 7, 8, 27, 30, 33, 47, 46, 45, 17, 14, 11) )
+      self._cycleCells(3 * dir, (6, 7, 8, 27, 30, 33, 47, 46, 45, 17, 14, 11) )
 
-   def moveFinv(self):      # F'
-       self.moveF(dir = -1)
+   def _moveFinv(self):      # F'
+       self._moveF(dir = -1)
 
-   def moveF2(self):
-       self.moveF(dir = 2)
+   def _moveF2(self):
+       self._moveF(dir = 2)
 
    # back
  
-   def moveB(self, dir = 1): #clockwise turn of BACK face (around #40)
-      self.__rotateFace(4, dir)   # face
+   def _moveB(self, dir = 1): #clockwise turn of BACK face (around #40)
+      self._rotateFace(4, dir)   # face
       # neighbors: 35,32,29| 2, 1, 0| 9,12,15|51,52,53| -> rotate by 3 for CW
-      self.__cycleCells(3 * dir, (35, 32, 29, 2, 1, 0, 9, 12, 15, 51, 52, 53) )
+      self._cycleCells(3 * dir, (35, 32, 29, 2, 1, 0, 9, 12, 15, 51, 52, 53) )
 
-   def moveBinv(self):       # B'
-       self.moveB(dir = -1)
+   def _moveBinv(self):       # B'
+       self._moveB(dir = -1)
 
-   def moveB2(self):
-       self.moveB(dir = 2)
+   def _moveB2(self):
+       self._moveB(dir = 2)
 
    # up
  
-   def moveU(self, dir = 1): #clockwise turn of UP face (around #4)
-      self.__rotateFace(0, dir)   # face
+   def _moveU(self, dir = 1): #clockwise turn of UP face (around #4)
+      self._rotateFace(0, dir)   # face
       # neighbors: 11,10, 9|38,37,36|29,28,27|20,19,18| -> rotate by 3 for CW
-      self.__cycleCells(3 * dir, (11, 10, 9, 38, 37, 36, 29, 28, 27, 20, 19, 18) )
+      self._cycleCells(3 * dir, (11, 10, 9, 38, 37, 36, 29, 28, 27, 20, 19, 18) )
 
-   def moveUinv(self):        # U'
-       self.moveU(dir = -1)
+   def _moveUinv(self):        # U'
+       self._moveU(dir = -1)
 
-   def moveU2(self):
-       self.moveU(dir = 2)
+   def _moveU2(self):
+       self._moveU(dir = 2)
 
    # down
  
-   def moveD(self, dir = 1): #clockwise turn of DOWN face (around #49)
+   def _moveD(self, dir = 1): #clockwise turn of DOWN face (around #49)
       # face
-      self.__rotateFace(5, dir)
+      self._rotateFace(5, dir)
       # neighbors: 15,16,17|24,25,26|33,34,35|42,43,44 -> rotate by 3 for CW
-      self.__cycleCells(3 * dir, (15, 16, 17, 24, 25, 26, 33, 34, 35, 42, 43, 44) )
+      self._cycleCells(3 * dir, (15, 16, 17, 24, 25, 26, 33, 34, 35, 42, 43, 44) )
 
-   def moveDinv(self):        # D'
-       self.moveD(dir = -1)
+   def _moveDinv(self):        # D'
+       self._moveD(dir = -1)
 
-   def moveD2(self):
-       self.moveD(dir = 2)    # D + D
+   def _moveD2(self):
+       self._moveD(dir = 2)    # D + D
 
    #
    # sequence of basic moves (L->1 R->2 F->3 B->4 U->5 D->6)
@@ -345,10 +333,10 @@ class RubikCube():
    # NOTE: we apply moves in left-to-right order
    #       whereas in operator notation, things go from right to left
 
-   int2move = { 1: moveL, 2: moveR, 3: moveF, 4: moveB, 5: moveU, 6: moveD,    # basic moves
-               -1: moveLinv, -2: moveRinv, -3: moveFinv,        # inverses 
-               -4: moveBinv, -5: moveUinv, -6: moveDinv,
-               11: moveL2, 12: moveR2, 13: moveF2, 14: moveB2, 15: moveU2, 16: moveD2  # double moves
+   int2move = { 1: _moveL,     2: _moveR,     3: _moveF, 4: _moveB, 5: _moveU, 6: _moveD,    # basic moves
+               -1: _moveLinv, -2: _moveRinv, -3: _moveFinv,        # inverses 
+               -4: _moveBinv, -5: _moveUinv, -6: _moveDinv,
+               11: _moveL2, 12: _moveR2, 13: _moveF2, 14: _moveB2, 15: _moveU2, 16: _moveD2  # double moves
               }
 
    def move(self, sequence, count = 1):   # apply sequence of moves
@@ -403,8 +391,11 @@ class RubikCube():
       invseq = RubikCube.invertMoves( RubikCube.string2moves(s) )
       return RubikCube.moves2string(invseq)
 
-   def __areCentersBad(tpl):
-      return tpl[4] != 5 or tpl[13] != 14 or tpl[22] != 23 or tpl[31] != 32 or tpl[40] != 41 or tpl[49] != 50 
+   def _areCentersBad(tpl):
+      for pos in (4, 13, 22, 31, 40, 49):
+         if tpl[pos] != pos:
+            return True
+      return False   
 
    def string2state(s): # FIXME: does not check rotation parity
       n = RubikCube.n
@@ -412,26 +403,26 @@ class RubikCube():
          raise("Incorrect string length " + str(len(s)))
       # only faces YBRGOW
       if s.count("Y") == 9:
-         lst = [RubikCube.color2int_map_FACES[v] for v in s]
+         lst = [RubikCube.facecolor2int_map[v] for v in s]
          # centers
          for i in RubikCube.center_indices:
-            lst[i] = RubikCube.color2center_map[lst[i]] + 1
+            lst[i] = RubikCube.color2center_map[lst[i]]
          # edges
          for (i, j) in RubikCube.edge_indices:
             (vi, vj) = RubikCube.color2edge_map[(lst[i], lst[j])]
-            lst[i], lst[j] = vi + 1, vj + 1
+            lst[i], lst[j] = vi, vj
          # corners
          for (i, j, k) in RubikCube.corner_indices:
             (vi, vj, vk) = RubikCube.color2corner_map[(lst[i], lst[j], lst[k])]
-            lst[i], lst[j], lst[k] = vi + 1, vj + 1, vk + 1
+            lst[i], lst[j], lst[k] = vi, vj, vk
       # 27 cells a-z,0-9,A-R
       else:  
          lst = [-1] * n
          for i in range(n):
             if lst[i] != -1:
                raise("Duplicate symbol " + str(len(s)))
-            lst[i] = RubikCube.color2int_map[s[i]]
-      if RubikCube.__areCentersBad(lst):
+            lst[i] = RubikCube.state2int_map[s[i]]
+      if RubikCube._areCentersBad(lst):
          raise("Centers wrong")
       return tuple(lst) 
   
@@ -451,7 +442,7 @@ class RubikCube():
 
    # give permutation that reaches current state from solved cube
    def state2permutation(self):
-      return tuple( [ (j - 1) for j in self.getState() ] )
+      return tuple( self.getState() )
 
    # convert a sequence of moves to a permutation
    def moves2permutation(sequence):
@@ -473,7 +464,7 @@ class RubikCube():
       state = self.state
       res = []
       for (i,j) in RubikCube.edge_indices:
-         pattern = (state[i] - 1, state[j] - 1)
+         pattern = (state[i], state[j])
          res.append(RubikCube.edge_permutation_map[pattern])
       return res
 
@@ -482,7 +473,7 @@ class RubikCube():
       state = self.state
       res = []
       for (i,j,k) in RubikCube.corner_indices:
-         pattern = (state[i] - 1, state[j] - 1, state[k] - 1)
+         pattern = (state[i], state[j], state[k])
          res.append(RubikCube.corner_permutation_map[pattern])
       return res
 
@@ -546,12 +537,17 @@ class RubikCube():
    #   http://people.math.harvard.edu/~jjchen/docs/Group%20Theory%20and%20the%20Rubik's%20Cube.pdf
    # and also using https://rubiks-cube-solver.com
    #
-   # edge positions, edge orientations, corner positions, corner orientations form commuting subgroups
-   # 
    # Parity of (unoriented) edge permutations and (unoriented) corner permutations must match.
    # (e.g., if precisely two edges are swapped (->odd), then at least two corners must be swapped as well.)
    #
    # Lemma 11.14 -> any two edge cubies can be flipped (orientations changed), with no other change to the cube.
+   #
+   # So, even edge permutations and even corner permutations form commuting subgroups (orientations may change)
+   # Edge orientations and corner orientations also form commuting subgroups.
+   #
+   # => one can reach a given state by adjusting edge permutations, then corner permutations, then edge orientations, 
+   #    and finally corner orientations
+   # 
    
    # cycle edges 0,1,2->1,2,0 (edges on same face)
    edge_cycle_012_str = "R2 U' B2 R2 B2 U B' F' U2 B F U2 R2"
@@ -561,22 +557,30 @@ class RubikCube():
    edge_flip_01_str = "R U F R2 F' R' U F2 R2 U2 F2 U2 R2 F U2 F"
   
 
+   # basic 3-corner cycles that generate even corner permutations
+   #
    # cycle corners 0,1,2->1,2,0 (corners on same face)
    # FIXME: 016 (two on same edge, two across same face)
    # FIXME: 025 (two across same face, another two across same face)
    corner_cycle_012_str = "R' L' D2 R U R' D2 R U' L"
+
    # storage for all 8*7*6 = 336 three-corner cycles - initialized after class code
    corner_cycles = [ [ [None]*8 for __ in range(8) ] for _ in range(8) ]
 
+   # basic 2-corner twists that generate corner orientations
+   #
    # twist corners 0,1 (on same edge), 0,2 (across same face), 0,6 (across cube)
    corner_twist_01_str = "D R D L' D' R' D R' F2 R B2 R' F2 R B2 L D2"
    corner_twist_02_str = "U2 F D B2 D' F' D2 B2 D' R2 D' R2 U F2 U F2" 
    corner_twist_06_str = "U B U' F2 U B' U' F2 D B2 D' F2 D B2 D' F2"
+
    # storage for all 8 * 7 = 28*2 two-corner twists - initialized after class code
    corner_twists = [ [None]*8  for _ in range(8) ]
 
-
+	
+####
 # additional initialization
+####
 
 # build all basic corner twists: neighbor twists -> 8*3/2 = 12
 #                                same-face diag twists -> 8*3/2 = 12
@@ -629,8 +633,8 @@ def TESTsuite(facesonly = False):
    cube.print(facesonly)
 
    # basic moves
-   lst = [("L", cube.moveL), ("R", cube.moveR), ("F", cube.moveF), 
-          ("B", cube.moveB), ("U", cube.moveU), ("D", cube.moveD)]
+   lst = [("L", cube._moveL), ("R", cube._moveR), ("F", cube._moveF), 
+          ("B", cube._moveB), ("U", cube._moveU), ("D", cube._moveD)]
    for (move, move_fn) in lst:
       # number of applications
       for i in range(1, 5):
